0851433b9cefad6abaaa01f2861d8324b5619001
tutorgaming <tutorgaming@gmail.com>
2015-04-17 21:00:33 +0700

    Merge Model File

--- models/group_member.js
+++ models/group_member.js
@@ -1,11 +1,12 @@
 var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
 
 var User = require("../models/user").User
 var Group	= require("../models/group").Group
 
 var GroupMemberSchema = new mongoose.Schema({
-	user:  {type : String , ref : User} ,
-	group_name: {type : String , ref : Group},
+	user:  {type : Schema.Types.ObjectId , ref : User} ,
+	group_name: {type : Schema.Types.ObjectId , ref : Group},
 	last_seen :{type : Date , default:Date.now}
 });
 
