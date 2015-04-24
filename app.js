a3cd621ad2202ed55baf992cd30151c4e9b94273
tutorgaming <tutorgaming@gmail.com>
2015-04-25 01:45:57 +0700

    user.leave implimented -User.method.leave implimented

--- app.js
+++ app.js
@@ -165,12 +165,31 @@ io.on('connection', function(socket){
 	socket.on('user.leave', function(data){
 		helper.SetData(data);
 		helper.IsLogin(function (UserObj) {
-			// leaving code here !
-
-			socket.emit(data._event, {
-				success: true,
-				err_msg: null,
+			var res = {
+									success: true,
+									err_msg: null
+			};
+			// find specific User
+			models.User.findOne({username: UserObj.username},function(err,results){
+				if(results){
+					//leave the user
+					results.leave(group_name,function(msg,parse){
+						 if(msg == 'success'){
+								res.success =true;
+								res.err_msg = [msg];
+							}else{
+								res.success = false;
+								res.err_msg = ['no_group_name'];
+							}
+							socket.emit(data._event, res);
+					});
+				}else{
+					res.success = false;
+					res.err_msg = ['no_user'];
+					socket.emit(data._event, res);
+				}
 			});
+
 		});
 	});
 
