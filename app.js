d5440078fe0e827233c2460e90a32686906658f2
Froztic <rxcn6615@gmail.com>
2015-04-26 00:09:05 +0900

    stupid error, my bad ;p

--- app.js
+++ app.js
@@ -484,18 +484,19 @@ io.on('connection', function(socket){
 				unread_msg: [],
 				success: true,
 				err_msg: null
-				models.Message.get_unread(data, UserObj, function(msg, unreadResults) {
-					if(unreadResults = 'unexpected') {
-						//Failed to function properly
-						returnObj.success = false;
-					} else {
-						//mikoto 
-						returnObj.success = true;
-						unread_msg = unreadResults;
-					}
-				});
-				socket.emit(data._event, returnObj);
+				
 			};
+			models.Message.get_unread(data, UserObj, function(msg, unreadResults) {
+				if(unreadResults = 'unexpected') {
+					//Failed to function properly
+					returnObj.success = false;
+				} else {
+					//mikoto 
+					returnObj.success = true;
+					unread_msg = unreadResults;
+				}
+			});
+			socket.emit(data._event, returnObj);
 			
 		});
 
