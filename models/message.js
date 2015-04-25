2b03069401adb3ac92fb07462baae6b341516cb2
Froztic <rxcn6615@gmail.com>
2015-04-26 00:22:21 +0900

    trying to make it work (2)

--- models/message.js
+++ models/message.js
@@ -10,7 +10,7 @@ var MessageSchema = new mongoose.Schema({
 
 var Message = mongoose.model('Message', MessageSchema);
 
-MessageSchema.methods.getunreadmsg = function getunreadmsg (data, UserObj, callback) {
+MessageSchema.statics.getunreadmsg = function getunreadmsg (data, UserObj, callback) {
 	console.log(UserObj.username + ' is calling get_unread');
 	//find group id
 	Group.findOne({group_name: data.group_name}, 'group_id', function (err, results) {
@@ -39,7 +39,6 @@ MessageSchema.methods.getunreadmsg = function getunreadmsg (data, UserObj, callb
 			//callback
 		}
 	});
-
 }
 
 module.exports = {
